f059afff1a104b6e96fa597d93e3fdd9aadf37ca
Potatoman50 <lowkim50@gmail.com>
2019-02-05 21:42:51 +0800

    Added Instagram images

--- src/components/PromotionImage.vue
+++ src/components/PromotionImage.vue
@@ -1,33 +1,43 @@
 <template>
-<div>
-    <div class="promotionImage" v-for="image in Images" :key="image['_id']">
-        <img class="image" :src="image.image.secure_url" :alt="image.name">
-    </div>
-</div>
-    
+  <div>
+    <Slider animation="fade" :duration="5000" :speed="1000" style="min-height:300px" :height="'800px'">
+        <SliderItem v-for="image in Images" :key="image['_id']">
+            <img class="img-fluid" :src="image.image.secure_url">
+            <!-- <img style="max-width:100%; height:auto;" :src="image.image.secure_url"> -->
+        </SliderItem>
+    </Slider>
+  </div>
 </template>
 
 <script>
-import Api from '@/config/Api'
+import Api from "@/config/Api";
+import { Slider, SliderItem } from "vue-easy-slider";
 export default {
-    name:'promotionImage',
-    data(){
-        return{
-            Images:{}
-        }
-    },
-    created(){
-        Api().get("/promotionImage")
-        .then(response => {
-            this.Images = response.data
-        })
-    }
-}
+  name: "promotionImage",
+  data() {
+    return {
+      Images: {},
+    };
+  },
+  created() {
+    Api()
+      .get("/promotionImage")
+      .then(response => {
+        this.Images = response.data;
+      });
+  },
+  components: {
+    Slider,
+    SliderItem
+  },
+  
+};
 </script>
 
 <style>
-.promotionImage{
-    text-align: center;
+.promotionImage {
+  text-align: center;
 }
+
 
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="home">
-    <div class="container">
+    <div class="container-fluid" style="max-width:1750px">
       <Images></Images>
+    </div>
+    <div class="container">
       <div class="ourStory">
         <h3>Our Story</h3>
         <p>THE started with the story of giving mothers bouquet. Florist Elf decided to finally buy a bouquet to pamper the mother and aunties and sisters (who were mothers) despite them always nagging at how expensive it was, or what a waste of money.
@@ -12,15 +14,37 @@
         >“Never wait till it is too late to give, that you regret even gifting one~....”</p>
       </div>
     </div>
+    <div class="container instaImgs">
+      <div class="row">
+        <div class="col-md-4" v-for="(iImages, index) in instaImages" :key="index">
+          <div class="card mb-4 shadow-sm">
+            <img :src="iImages" :key="index">
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import promotionImage from "@/components/PromotionImage";
+import Api from "@/config/Api";
 export default {
   name: "home",
+  data() {
+    return {
+      instaImages: []
+    };
+  },
   components: {
     Images: promotionImage
+  },
+  created() {
+    Api()
+      .get("/insta")
+      .then(response => {
+        this.instaImages = response.data;
+      });
   }
 };
 </script>
@@ -43,7 +67,17 @@ export default {
   font-size: 27px;
 }
 
-.home h3{
-  font-family: 'October Twilight';
+.home h3 {
+  font-family: "October Twilight";
+}
+
+.instaImgs {
+  display: flex;
+}
+
+.instaImgs > img {
+  width: 200px;
+  height: 15vh;
+  margin-left: 2px;
 }
 </style>
